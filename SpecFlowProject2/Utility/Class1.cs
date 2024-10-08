4e36a50277d3e8da8eeb88ad5c8cbb456b0b87b2
Swati More <Swati.More@nice.com>
2024-10-08 17:22:29 +0530

    pull

---
+++ SpecFlowProject2/Utility/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProject2.Utility
+{
+    internal class Class1
+    {
+    }
+}
